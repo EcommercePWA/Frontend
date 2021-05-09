ade0812fef0bbc38fd644c6292c9b2853b6872d9
Sean <e0406739@u.nus.edu>
2021-05-09 23:25:17 +0800

    add compatibility with material-ui

---
+++ styles/theme.ts
@@ -0,0 +1,20 @@
+// styles/theme.ts
+
+import { createMuiTheme } from '@material-ui/core/styles';
+
+// Create a theme instance.
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#6772E5'
+    },
+    secondary: {
+      main: '#87BBFD' // omitting light and dark will calculate from main
+    }
+  },
+  typography: {
+    fontFamily: 'Roboto'
+  }
+});
+
+export default theme;
